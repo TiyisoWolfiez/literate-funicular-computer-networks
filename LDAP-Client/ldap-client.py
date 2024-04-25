976101c22e250ad05583ff847a51c98fa0372399
tebogoyungmercykay <u20748052@tuks.co.za>
2024-04-24 23:12:39 -0400

    BackUp filr for DNS Info

---
+++ LDAP-Client/ldap-client.py
@@ -0,0 +1,106 @@
+import ldap3
+
+class LdapClient:
+    def __init__(self, server, user, password):
+        self.server = ldap3.Server(server)
+        self.conn = ldap3.Connection(self.server, user, password, auto_bind=True)
+
+    def query(self, base_dn, query):
+        self.conn.search(base_dn, query, attributes=ldap3.ALL_ATTRIBUTES)
+        return self.conn.entries
+
+
+def query_all_tld(client, tld = 'za'):
+    entries = client.query('dc=za', '(objectClass=*)')
+    print("\n\n----------------------------------------")
+    print(f"--- All (.{tld}) Top Level Domain Entries: ------")
+    print("----------------------------------------")
+    for entry in entries:
+        print(entry)
+        
+
+def query_all_sld(client, sld = 'co'):
+    entries = client.query(f'ou={sld},dc=za', '(objectClass=*)')
+    print("\n\n----------------------------------------")
+    print(f"--- All (.{sld})) Second Level Domain Entries: ---")
+    print("----------------------------------------")
+    for entry in entries:
+        print(entry)
+
+        
+def query_all_org(client, org = 'tebogoyungmercykay', sld = 'co'):
+    entries = client.query(f'o={org},ou={sld},dc=za', '(objectClass=*)')
+    print("\n\n----------------------------------------")
+    print(f"--- All (.{org}.{sld}) Organisations Entries: ---------")
+    print("----------------------------------------")
+    for entry in entries:
+        print(entry)
+        
+
+def query_all_org_info(client, org = 'tebogoyungmercykay', sld = 'co'):
+    entries = client.query(f'o={org},ou={sld},dc=za', '(objectClass=dnsDomain)')
+    print("\n\n----------------------------------------")
+    print(f"--- All Info (.{org}.{sld}) Organisations Entries: ---------")
+    print("----------------------------------------")
+    for entry in entries:
+        print(entry)
+        print("\n")
+        if 'aRecord' in entry:
+            print("A Record:", entry.aRecord)
+        if 'nSRecord' in entry:
+            print("NS Record:", entry.nSRecord)
+        if 'mXRecord' in entry:
+            print("MX Record:", entry.mXRecord)
+        print("\n")
+
+
+def add_dns_info(client, org='tebogoyungmercykay', sld = 'co', a_record='127.0.0.1', ns_record='ns.example.com', mx_record='mx.example.com'):
+    dn = f'o={org},ou={sld},dc=za'
+    changes = {
+        'aRecord': [(ldap3.MODIFY_REPLACE, [a_record])],
+        'nSRecord': [(ldap3.MODIFY_REPLACE, [ns_record])],
+        'mXRecord': [(ldap3.MODIFY_REPLACE, [mx_record])]
+    }
+    client.conn.modify(dn, changes)
+
+
+def add_sld(client, sld='gov'):
+    dn = f'ou={sld},dc=za'
+    object_class = ['top', 'organizationalUnit']
+    attributes = {
+        'ou': sld
+    }
+    client.conn.add(dn, object_class, attributes)
+
+
+def add_org(client, org='uj', sld='ac'):
+    dn = f'o={org},ou={sld},dc=za'
+    object_class = ['top', 'organization']
+    attributes = {
+        'o': org
+    }
+    client.conn.add(dn, object_class, attributes)
+
+
+# Usage
+client = LdapClient('ldap://localhost:389', 'cn=admin,dc=za', 'password')
+add_dns_info(client)
+
+# Some Queries
+query_all_tld(client)
+query_all_sld(client)
+query_all_sld(client, 'ac')
+query_all_sld(client, 'gov')
+query_all_org(client)
+query_all_org(client, org='up', sld = 'ac')
+query_all_org_info(client)
+query_all_org_info(client, org='up', sld = 'ac')
+
+# Add Entries
+add_sld(client, 'drink')
+add_org(client, 'sprite', 'drink')
+add_org(client, 'code', 'drink')
+
+print("-----------------------------------------")
+print("--- Testing Some More Inputs ------------")
+query_all_sld(client, 'drink')
