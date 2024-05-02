5373bc271b4e6390d5e02a43b04340dfe9aeef98
tebogoyungmercykay <u20748052@tuks.co.za>
2024-05-02 20:37:45 +0000

    Updated Code

--- SMTP/resego/quiz_server.py
+++ SMTP/resego/quiz_server.py
@@ -53,69 +53,59 @@ def load_questions(filename):
     return questions
 
 def send_email(sender_email, receiver_email, host, port, password, score):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
-        socket.connect((host, port))
-
-        ehlo_command = 'EHLO {}\r\n'.format(host)
-        socket.sendall(ehlo_command.encode())
-        response = socket.recv(1024)
-        print(response.decode())
-
-        if b'STARTTLS' in response:
-            starttls_command = 'STARTTLS\r\n'
-            socket.sendall(starttls_command.encode())
-            response = socket.recv(1024)
-            print(response.decode())
-
-            ssl_socket = ssl.wrap_socket(socket, ssl_version=ssl.PROTOCOL_TLSv1_2)
-
+    # Establish a TCP connection to the SMTP server
+    with socket.create_connection((host, port)) as server_socket:
+        # Wrap the socket with SSL/TLS
+        with ssl.create_default_context().wrap_socket(server_socket, server_hostname=host) as ssl_socket:
+            # Send EHLO command
             ehlo_command = 'EHLO {}\r\n'.format(host)
             ssl_socket.sendall(ehlo_command.encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
 
+            # Send authentication credentials
             auth_command = 'AUTH LOGIN\r\n'
             ssl_socket.sendall(auth_command.encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
-
             username_b64 = base64.b64encode(sender_email.encode()).decode()
             password_b64 = base64.b64encode(password.encode()).decode()
             ssl_socket.sendall((username_b64 + '\r\n').encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             ssl_socket.sendall((password_b64 + '\r\n').encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
 
+            # Send MAIL FROM command
             mail_from_command = f"MAIL FROM: <{sender_email}>\r\n"
             ssl_socket.sendall(mail_from_command.encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
 
+            # Send RCPT TO command
             rcpt_to_command = f"RCPT TO: <{receiver_email}>\r\n"
             ssl_socket.sendall(rcpt_to_command.encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
 
+            # Send DATA command
             data_command = 'DATA\r\n'
             ssl_socket.sendall(data_command.encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
             
+            # Send email content
             header = 'Subject: Your Score\r\n'
             message = 'Your Score is: {}/3.\r\n'.format(score)
             end_of_message = '\r\n.\r\n'
             ssl_socket.sendall((header + message + end_of_message).encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
 
+            # Send QUIT command
             quit_command = 'QUIT\r\n'
             ssl_socket.sendall(quit_command.encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
-
-            ssl_socket.close()
-
-        socket.close()
 
 class QuizHandler(BaseHTTPRequestHandler):
     questions = load_questions('questions.txt')
--- SMTP/temp/my_email.py
+++ SMTP/temp/my_email.py
@@ -7,80 +7,71 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def send_email(sender_email, receiver_email, host, port, password, score):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
-        socket.connect((host, port))
-
-        ehlo_command = 'EHLO {}\r\n'.format(host)
-        socket.sendall(ehlo_command.encode())
-        response = socket.recv(1024)
-        print(response.decode())
-
-        if b'STARTTLS' in response:
-            starttls_command = 'STARTTLS\r\n'
-            socket.sendall(starttls_command.encode())
-            response = socket.recv(1024)
-            print(response.decode())
-
-            ssl_socket = ssl.wrap_socket(socket, ssl_version=ssl.PROTOCOL_TLSv1_2)
-
+    # Establish a TCP connection to the SMTP server
+    with socket.create_connection((host, port)) as server_socket:
+        # Wrap the socket with SSL/TLS
+        with ssl.create_default_context().wrap_socket(server_socket, server_hostname=host) as ssl_socket:
+            # Send EHLO command
             ehlo_command = 'EHLO {}\r\n'.format(host)
             ssl_socket.sendall(ehlo_command.encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
 
+            # Send authentication credentials
             auth_command = 'AUTH LOGIN\r\n'
             ssl_socket.sendall(auth_command.encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
-
             username_b64 = base64.b64encode(sender_email.encode()).decode()
             password_b64 = base64.b64encode(password.encode()).decode()
             ssl_socket.sendall((username_b64 + '\r\n').encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             ssl_socket.sendall((password_b64 + '\r\n').encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
 
+            # Send MAIL FROM command
             mail_from_command = f"MAIL FROM: <{sender_email}>\r\n"
             ssl_socket.sendall(mail_from_command.encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
 
+            # Send RCPT TO command
             rcpt_to_command = f"RCPT TO: <{receiver_email}>\r\n"
             ssl_socket.sendall(rcpt_to_command.encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
 
+            # Send DATA command
             data_command = 'DATA\r\n'
             ssl_socket.sendall(data_command.encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
             
+            # Send email content
             header = 'Subject: Your Score\r\n'
             message = 'Your Score is: {}/3.\r\n'.format(score)
             end_of_message = '\r\n.\r\n'
             ssl_socket.sendall((header + message + end_of_message).encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
 
+            # Send QUIT command
             quit_command = 'QUIT\r\n'
             ssl_socket.sendall(quit_command.encode())
-            response = socket.recv(1024)
+            response = ssl_socket.recv(1024)
             print(response.decode())
 
-            ssl_socket.close()
+if __name__ == "__main__":
+    host = os.getenv("HOST")
+    port = os.getenv("PORT")
+    username = os.getenv("SENDER_EMAIL")
+    password = os.getenv("EMAIL_PASSWORD")
+    receiver_email = os.getenv("RECEIVER_EMAIL")
 
-        socket.close()
+    send_mail = True
+    score = 4
 
-host = os.getenv("HOST")
-port = os.getenv("PORT")
-username = os.getenv("SENDER_EMAIL")
-password = os.getenv("EMAIL_PASSWORD")
-receiver_email = os.getenv("RECEIVER_EMAIL")
-
-send_mail = False
-score = 4
-
-if send_mail:
-    send_email(username, receiver_email, host, port, password, score)
-else:
-    print('Results will be released after the IMY310 ones (^_^)')
+    if send_mail:
+        send_email(username, receiver_email, host, port, password, score)
+    else:
+        print('Results will be released after the IMY310 ones (^_^)')
