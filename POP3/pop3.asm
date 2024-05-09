01889bfd196ce97adef1c701a677626fedb81459
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-09 09:42:36 +0200

    Update pop3.asm
    implementation of pop3.asm

--- POP3/pop3.asm
+++ POP3/pop3.asm
@@ -1,1 +1,144 @@
+nasm
+.section .data
+    server:
+        .asciz "%s"
+    port:
+        .asciz "%s"
+    username:
+        .asciz "%s"
+    password:
+        .asciz "%s"
+    response:
+        .space 1024
+    num_messages:
+        .long 0
 
+.section .text
+.globl _start
+
+_start:
+    # Load environment variables
+    call load_dotenv
+
+    # Set up the TCP connection
+    movl $socket, %eax
+    movl $2, %ebx # AF_INET
+    movl $1, %ecx # SOCK_STREAM
+    movl $0, %edx # Protocol (default)
+    int $0x80
+    movl %eax, %edi # Save socket descriptor
+
+    # Wrap the socket in an SSL context
+    call create_ssl_context
+    movl %eax, %esi # Save SSL socket descriptor
+
+    # Receive the server greeting
+    movl %esi, %eax
+    movl $1024, %ebx
+    movl $response, %ecx
+    int $0x80
+    movl %eax, %edx
+    movl $response, %ecx
+    call print_response
+
+    # Send the username and password
+    movl %esi, %eax
+    movl $username, %ebx
+    call send_credentials
+    movl %esi, %eax
+    movl $password, %ebx
+    call send_credentials
+
+    # Get the mailbox statistics
+    movl %esi, %eax
+    movl $STAT, %ebx
+    call send_command
+    movl %esi, %eax
+    movl $response, %ebx
+    call receive_response
+    movl $response, %ecx
+    call print_response
+
+    # List the messages on the server
+    movl %esi, %eax
+    movl $LIST, %ebx
+    call send_command
+    movl %esi, %eax
+    movl $response, %ebx
+    call receive_response
+    movl $response, %ecx
+    call print_response
+    movl $response, %eax
+    call get_num_messages
+
+    # Retrieve the headers of each message
+    movl $1, %ecx
+message_loop:
+    cmpl %ecx, %eax
+    jg message_loop_end
+    movl %esi, %ebx
+    pushl %ecx
+    call retrieve_message
+    popl %ecx
+    incl %ecx
+    jmp message_loop
+message_loop_end:
+
+    # Log out of the server
+    movl %esi, %eax
+    movl $QUIT, %ebx
+    call send_command
+    movl %esi, %eax
+    movl $response, %ebx
+    call receive_response
+    movl $response, %ecx
+    call print_response
+
+    # Close the socket
+    movl %esi, %eax
+    int $0x80
+
+    # Exit the program
+    movl $1, %eax
+    xorl %ebx, %ebx
+    int $0x80
+
+load_dotenv:
+    # Load environment variables
+    # ...
+    ret
+
+create_ssl_context:
+    # Create an SSL context
+    # ...
+    ret
+
+send_credentials:
+    # Send username or password
+    # ...
+    ret
+
+send_command:
+    # Send a command to the server
+    # ...
+    ret
+
+receive_response:
+    # Receive a response from the server
+    # ...
+    ret
+
+print_response:
+    # Print the server response
+    # ...
+    ret
+
+get_num_messages:
+    # Get the number of messages on the server
+    # ...
+    ret
+
+retrieve_message:
+    # Retrieve the headers of a message
+    # ...
+    ret
